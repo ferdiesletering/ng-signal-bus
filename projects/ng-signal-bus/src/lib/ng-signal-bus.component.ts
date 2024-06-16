97064f344a4baffa8684baaebd6a99715c58935a
Ferdie Sletering <s_ferdie@hotmail.com>
2024-06-16 21:08:50 +0200

    chore: Add ng-signal-bus library files and configuration

---
+++ projects/ng-signal-bus/src/lib/ng-signal-bus.component.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'lib-ng-signal-bus',
+  standalone: true,
+  imports: [],
+  template: `
+    <p>
+      ng-signal-bus works!
+    </p>
+  `,
+  styles: ``
+})
+export class NgSignalBusComponent {
+
+}
---
+++ projects/ng-signal-bus/src/public-api.ts
@@ -0,0 +1,6 @@
+/*
+ * Public API Surface of ng-signal-bus
+ */
+
+export * from './lib/ng-signal-bus.service';
+export * from './lib/ng-signal-bus.component';
